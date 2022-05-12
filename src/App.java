281572418ae6613157e602d870fbaf13937f4751
Hayden <asianhaydenxd@gmail.com>
2022-05-11 23:41:23 -0400

    Implement tape and cells

--- src/App.java
+++ src/App.java
@@ -1,3 +1,42 @@
+import java.util.ArrayList;
+
+class Cell {
+    int value = 0;
+
+    void increment() {
+        value += 1;
+        if (value > 255) value = 0;
+    }
+
+    void decrement() {
+        value -= 1;
+        if (value < 0) value = 255;
+    }
+}
+
+class Tape {
+    ArrayList<Cell> tape = new ArrayList<Cell>();
+    private int pointer = 0;
+
+    void shiftRight() {
+        this.pointer += 1;
+        if (this.pointer == this.tape.size()) this.tape.add(new Cell());
+    }
+    
+    void shiftLeft() throws Exception {
+        this.pointer -= 1;
+        if (this.pointer < 0) throw new Exception("Pointer out of range");
+    }
+
+    void increment() {
+        this.tape.get(this.pointer).increment();
+    }
+
+    void decrement() {
+        this.tape.get(this.pointer).decrement();
+    }
+}
+
 class Interpreter {
     String code;
 
