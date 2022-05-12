bfc2fe0fcbff55b49b528abed193fe190ada7a60
Hayden <asianhaydenxd@gmail.com>
2022-05-11 23:24:35 -0400

    Add interpreter class

--- src/App.java
+++ src/App.java
@@ -1,3 +1,11 @@
+class Interpreter {
+    String code;
+
+    public Interpreter(String code) {
+        this.code = code;
+    }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
